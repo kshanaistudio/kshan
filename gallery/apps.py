a1e5ec93718ebf73e71463a7da9f9bdd57decbda
thepranit10 <thepranit.19@gmail.com>
2026-09-07 13:45:03 +0530

    Add trial account p/p and upload auto-retry resilience for network glitches

--- gallery/apps.py
+++ gallery/apps.py
@@ -1,5 +1,35 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
+def create_default_trial_user(sender, **kwargs):
+    try:
+        from django.contrib.auth.models import User
+        from .models import PhotographerProfile
+        
+        # Trial user: username: p, password: p
+        user, created = User.objects.get_or_create(username='p')
+        if created or not user.has_usable_password() or not user.check_password('p'):
+            user.set_password('p')
+            user.is_staff = True
+            user.is_superuser = True
+            user.save()
+            
+        profile, _ = PhotographerProfile.objects.get_or_create(
+            user=user,
+            defaults={
+                'studio_name': 'Trial Studio (P)',
+                'display_name': 'Pranit Studio',
+                'phone': '+919970343404',
+                'city': 'Mumbai',
+                'event_credits': 99
+            }
+        )
+    except Exception:
+        pass
 
 class GalleryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'gallery'
+
+    def ready(self):
+        post_migrate.connect(create_default_trial_user, sender=self)
